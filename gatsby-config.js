543d4d473018e66eda2445e54919bc97762f968a
McManus <macmanuch@hotmail.fr>
2020-04-30 14:51:56 -0400

    Improve site descripption

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-countries-of-the-world`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/earth-icon.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,

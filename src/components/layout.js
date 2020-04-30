78337c4fbcde075f12868859465a4c99fce39170
McManus <macmanuch@hotmail.fr>
2020-04-30 14:40:41 -0400

    improve layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  console.log(data)
-
   return (
     <>
       <main>{children}</main>
